903b09a79c33cfa747d1d7203f57869a1705265e
Caresle <carabesle@gmail.com>
2023-05-08 21:29:37 -0600

    Add problem 5 a

---
+++ src/simple-training/A-problems/5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m, c = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (
+                i * i + j == n
+                && i + j * j == m
+            )
+                c++;
+        }
+    }
+    cout << c << "\n";
+}
